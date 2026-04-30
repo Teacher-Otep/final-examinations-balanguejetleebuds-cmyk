68922c9c8173b4cd1c2ed7286e12ce408a6c7709
balanguejetleebuds-cmyk <balanguejetleebuds@gmail.com>
2026-04-30 15:45:46 +0800

    Update test.php

--- includes/test.php
+++ includes/test.php
@@ -1,4 +1,18 @@
 <?php
-echo'<p>Hello world</p>';
+include 'db.php';
 
+if (isset($_POST['btn_update'])) {
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $middlename = $_POST['middlename'];
+    $address = $_POST['address'];
+    $contact = $_POST['contact'];
+    
+    $stmt = $pdo->prepare("UPDATE students SET name = ?, surname = ?, middlename = ?, address = ?, contact_number = ? WHERE id = ?");
+    $stmt->execute([$name, $surname, $middlename, $address, $contact, $id]);
+    
+    header("Location: ../public/index.php?status=updated");
+    exit();
+}
 ?>
